bfeb5f959cdd271ef32b434b6e5f198413b1a5d3
PixelParlor CI <ci@pocketarcade.app>
2026-06-10 02:09:11 +0000

    [version-bump] Bump to v2.1.16 (code 63)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.pocketarcade"
         minSdk = 26
         targetSdk = 34
-        versionCode = 62
-        versionName = "2.1.15"
+        versionCode = 63
+        versionName = "2.1.16"
 
         buildConfigField("String", "ADMOB_APP_ID",        "\"ca-app-pub-3940256099942544~3347511713\"")
         buildConfigField("String", "BANNER_AD_UNIT_ID",   "\"ca-app-pub-3940256099942544/6300978111\"")
